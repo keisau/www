67e6cab608eaf5627c40036fbedaa106d7f68e16
Keisau CHING <keisau.ching@pierresaux.me>
2017-04-12 17:32:01 +0800

    sidebar contents update, styling

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -4,20 +4,29 @@ div#sidebarContainer
     a(href='/') Keisau CHING
 
   div#contacts
-    a(href='mailto:keisau.ching@pierresaux.me')
-      i(class='fa fa-envelope', aria-hidden='true')
-      | keisau.ching@pierresaux.me
+    a(href='//facebook.com/keisau.ching')
+      i(class='fa fa-facebook-square', aria-hidden='true')
+      | keisau.ching
     br
     a(href='//github.com/pierresaux')
       i(class='fa fa-github', aria-hidden='true')
       | github.com/pierresaux
+    br
+    a(href='mailto:keisau.ching@pierresaux.me')
+      i(class='fa fa-envelope', aria-hidden='true')
+      | keisau.ching@pierresaux.me
 
-  p #SoftwareEngineer
+
+  p #Software Engineer
     i(class='fa fa-code', aria-hidden='true')
-  p #ChineseCalligraphy
+  p #Chinese Calligraphy
     i(class='fa fa-pencil', aria-hidden='true')
+  p #IPSC Action Air
+    i(class='fa fa-bullseye', aria-hidden='true')
   p #Philosophy
     i(class='fa fa-comment', aria-hidden='true')
+  p #Hong Kong
+    i(class='fa fa-globe', aria-hidden='true')
 
   div#divider.responsiveHidden
     div.bubble
